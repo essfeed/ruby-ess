a4031fe34c7ce2d4086f0f94d5a2d46fd54a2c6d
Marjan Povolni <marian.povolny@gmail.com>
2013-04-26 15:36:17 +0200

    Channel now supports all it's available children tags

--- lib/ess/channel.rb
+++ lib/ess/channel.rb
@@ -1,20 +1,46 @@
 
 module ESS
   class Channel
-    attr_reader :title
 
-    def title= new_title
-      if new_title.class == String
-        @title = Element.new(new_title)
-      elsif new_title.class == Element
-        @title = new_title
-      else
-        raise TypeError.new "only strings and #{Element} objects can be used to set the title"
+    def initialize
+      @children = {}
+    end
+
+    def self.define_element_reader_method_for element_name
+      define_method(element_name) do
+        get_child(element_name)
       end
+    end
+
+    def self.define_element_writer_method_for element_name
+      define_method("#{element_name}=".to_sym) do |value|
+        set_child(element_name, value)
+      end
+    end
+
+    [:title, :link, :id, :published, :updated, :generator, :rights, :feed].each do |element_name|
+      define_element_reader_method_for element_name
+      define_element_writer_method_for element_name
     end
 
     def valid?
     end
+
+    private
+
+      def get_child child_name
+        return @children[child_name] if @children.include? child_name
+      end
+
+      def set_child child_name, new_value
+        if new_value.class == String
+          @children[child_name] = Element.new(new_value)
+        elsif new_value.class == Element
+          @children[child_name] = new_value
+        else
+          raise TypeError.new "only strings and #{Element} objects can be used to set the #{child_name}"
+        end
+      end
   end
 end
 
--- spec/ess/channel_spec.rb
+++ spec/ess/channel_spec.rb
@@ -14,34 +14,38 @@ module ESS
       end
     end
 
-    describe '#title=' do
-      it 'should accept a string' do
-        lambda { channel.title = "An example title" }.should_not raise_error
-      end
+    [:title, :link, :id, :published, :updated, :generator, :rights, :feed].each do |element_name|
+      describe "##{element_name}=" do
+        let(:writer_method) { "#{element_name}=".to_sym }
 
-      it 'should accept an element object' do
-        lambda { channel.title = Element.new }.should_not raise_error
-      end
-
-      it 'should not allow anything else beside strings and Elements' do
-        expect { channel.title = [] }.to raise_error(TypeError)
-      end
-    end
-
-    describe '#title' do
-      it 'should return nil if no title was specified' do
-        channel.title.should be_nil
-      end
-
-      context 'when title was already set' do
-        before(:each) { channel.title = "An example title" }
-
-        it 'should return an instance of Element' do
-          channel.title.should be_an_instance_of(Element)
+        it 'should accept a string' do
+          lambda { channel.send(writer_method, "Some text") }.should_not raise_error
         end
 
-        it 'should return an instance of Element with the message in text' do
-          channel.title.text.should == "An example title"
+        it 'should accept an element object' do
+          lambda { channel.send(writer_method, Element.new) }.should_not raise_error
+        end
+
+        it 'should not allow anything else beside strings and Elements' do
+          expect { channel.send(writer_method, []) }.to raise_error(TypeError)
+        end
+      end
+
+      describe "##{element_name}" do
+        it 'should return nil if the element was not set' do
+          channel.send(element_name).should be_nil
+        end
+
+        context "when #{element_name} was already set" do
+          before(:each) { channel.send("#{element_name}=".to_sym, "Some text") }
+
+          it 'should return an instance of Element' do
+            channel.send(element_name).should be_an_instance_of(Element)
+          end
+
+          it 'should return an instance of Element with the message in text' do
+            channel.send(element_name).text.should == "Some text"
+          end
         end
       end
     end
