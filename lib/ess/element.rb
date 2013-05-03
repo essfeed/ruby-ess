2f7c8a0e1ebedcca78136faacc248ebdaccf8594
Marjan Povolni <marian.povolny@gmail.com>
2013-05-03 18:57:09 +0200

    Described basic Element and how text should work

--- lib/ess/element.rb
+++ lib/ess/element.rb
@@ -1,47 +1,22 @@
 module ESS
   class Element
-    attr_reader :text
 
-    def initialize text=""
-      @text = text
-      @children = {}
+    def initialize dtd
+      raise "Bad DTD: no attributes description" if !dtd.include? :attributes
+      raise "Bad DTD: no tags description" if !dtd.include? :tags
     end
 
-    def text= new_text
-      if new_text.class == String
-        @text = new_text
+    def text text=nil
+      if text.nil?
+        return @text ||= ""
+      elsif text.class == String
+        @text = text
       else
-        raise TypeError.new "only strings can be used as a value for the element text"
+        raise TypeError.new "Only strings can be used as a value for the element text"
       end
-    end
-
-    def valid?
     end
 
     private
-
-      def self.add_child_elements *child_names
-        child_names.each do |name|
-          define_element_reader_method_for name
-          define_element_writer_method_for name
-        end
-      end
-
-      def self.define_element_reader_method_for element_name
-        define_method(element_name) do
-          get_child(element_name)
-        end
-      end
-
-      def self.define_element_writer_method_for element_name
-        define_method("#{element_name}=".to_sym) do |value|
-          set_child(element_name, value)
-        end
-      end
-
-      def get_child child_name
-        return @children[child_name] if @children.include? child_name
-      end
 
       def set_child child_name, new_value
         if new_value.class == String
--- spec/ess/element_spec.rb
+++ spec/ess/element_spec.rb
@@ -3,45 +3,78 @@ require 'spec_helper'
 module ESS
   describe Element do
 
-    let(:element) { Element.new }
-
-    it 'should allow setting of text' do
-      element.should respond_to(:text=)
-    end
-
-    it 'should have a text method' do
-      element.should respond_to(:text)
-    end
-
-    it 'should allow setting of text using new' do
-      lambda { Element.new "Some text" }.should_not raise_error
-      Element.new("Some text").text.should == "Some text"
-    end
-
-    describe '#text=' do
-      it 'should allow string arguments' do
-        lambda { element.text = "Some text" }.should_not raise_error
+    describe '#new' do
+      it 'should require a DTD argument' do
+        expect { Element.new }.to raise_error
       end
 
-      it 'should not allow objects except for strings' do
-        expect { element.text = [] }.to raise_error(TypeError)
+      it 'should require that the DTD argument have "attributes" and "tags" keys' do
+        expect { Element.new({}) }.to raise_error
+        expect { Element.new({:attributes => ""}) }.to raise_error
+        expect { Element.new({:tags => ""}) }.to raise_error
+      end
+
+      it 'should accept a hash with both :attributes and :tags keys' do
+        lambda {
+          Element.new({:attributes => "", :tags => ""})
+        }.should_not raise_error
       end
     end
 
     describe '#text' do
-      context 'when no text was set' do
-        it 'should return an empty string' do
-          element.text.class.should == String
-          element.text.should == ''
+      let(:element) { Element.new({:attributes => "", :tags => ""}) }
+
+      context 'when text was not preset' do
+        context 'without parameters' do
+          it 'should return an empty string' do
+            element.text.should == ""
+          end
+        end
+        context 'with parameters' do
+          it 'should accept one parameter' do
+            lambda {
+              element.text("Example text")
+            }.should_not raise_error
+          end
+
+          it 'should accept only strings' do
+            expect { element.text({}) }.to raise_error
+          end
+
+          it 'should set the text value of the element to that parameter' do
+            element.text "Example text"
+            element.text.should == "Example text"
+          end
         end
       end
-      context 'when text was set' do
-        it 'should return that text' do
-          element.text = "Some text"
-          element.text.should == "Some text"
+
+      context 'when text was preset' do
+        before(:each) { element.text "Example text" }
+
+        context 'without parameters' do
+          it 'should return that text' do
+            element.text.should == "Example text"
+          end
+        end
+        context 'with parameters' do
+          it 'should accept one parameter' do
+            lambda {
+              element.text("Example text")
+            }.should_not raise_error
+          end
+
+          it 'should accept only strings' do
+            expect { element.text({}) }.to raise_error
+          end
+
+          it 'should set the text value of the element to that parameter' do
+            element.text "Example text"
+            element.text.should == "Example text"
+          end
         end
       end
     end
+
   end
 end
 
