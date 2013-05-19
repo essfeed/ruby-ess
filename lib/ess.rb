11e580e81883d518805e3900e12f3308a4cf3901
Marjan Povolni <marian.povolny@gmail.com>
2013-05-20 01:31:42 +0200

    Removed channel class

--- lib/ess.rb
+++ lib/ess.rb
@@ -1,7 +1,6 @@
 require "ess/version"
 require "ess/dtd"
 require "ess/element"
-require "ess/channel"
 require "ess/maker"
 require "ess/helpers"
 require "ess/postprocessing"
--- lib/ess/channel.rb
+++
@@ -1,8 +0,0 @@
-module ESS
-  class Channel < Element
-    def initialize
-      super :channel, ESS::DTD::CHANNEL
-    end
-  end
-end
-
