1a620fd448d120c370bf4558ac5e0f36a5f2045c
Marjan Povolni <marian.povolny@gmail.com>
2013-05-30 15:03:03 +0200

    Added #find_coming and #find_between methods to ESS class.
    They both support only standalone and recurrent date items for now,
    while taking into account the "unit" and "interval" attributes into
    account.

--- lib/ess/ess.rb
+++ lib/ess/ess.rb
@@ -1,8 +1,135 @@
+require 'time'
+
 module ESS
   class ESS < Element
     def initialize
       super :ess, DTD::ESS
     end
+
+    def find_coming n=10
+      feeds = []
+      channel.feed_list.each do |feed|
+        feed.dates.item_list.each do |item|
+          if item.type_attr == "standalone"
+            feeds << { :time => Time.parse(item.start.text!), :feed => feed }
+          elsif item.type_attr == "recurrent"
+            moments = parse_recurrent_date_item(item)
+            moments.sort! { |x, y| x <=> y }
+            moments.each do |moment|
+              feeds << { :time => moment, :feed => feed }
+            end
+          else
+            raise DTD::InvalidValueError, "the \"#{item.type_attr}\" is not valid for a date item type attribute"
+          end
+        end
+      end
+      feeds.sort! { |x, y| x[:time] <=> y[:time] }
+      return feeds[0..n-1]
+    end
+
+    def find_between start_time, end_time
+      feeds = []
+      channel.feed_list.each do |feed|
+        feed.dates.item_list.each do |item|
+          if item.type_attr == "standalone"
+            feed_start_time = Time.parse(item.start.text!)
+            if feed_start_time.between?(start_time, end_time)
+              feeds << { :time => feed_start_time, :feed => feed }
+            end
+          elsif item.type_attr == "recurrent"
+            moments = parse_recurrent_date_item(item)
+            moments.each do |moment|
+              if moment.between?(start_time, end_time)
+                feeds << { :time => moment, :feed => feed }
+              end
+            end
+          else
+            raise DTD::InvalidValueError, "the \"#{item.type_attr}\" is not valid for a date item type attribute"
+          end
+        end
+      end
+      feeds.sort! { |x, y| x[:time] <=> y[:time] }
+    end
+
+    private
+   
+      def parse_recurrent_date_item item
+        first = Time.parse(item.start.text!)
+        limit = item.limit_attr.to_i
+        all = [first]
+        (2..limit).each do |n|
+          all << calc_nth_event(n, first, item.unit_attr, item.interval_attr)
+        end
+        all
+      end
+
+      def calc_nth_event n, first, unit, interval
+        next_event = first
+        interval = 1 if interval.empty?
+        repeat = (n-1) * interval.to_i
+        case unit
+        when "year"
+          repeat.times { next_event = inc_year(next_event) }
+        when "month"
+          repeat.times { next_event = inc_month(next_event) }
+        when "week"
+          repeat.times { next_event = inc_week(next_event) }
+        when "day"
+          repeat.times { next_event = inc_day(next_event) }
+        when "hour"
+          repeat.times { next_event = inc_hour(next_event) }
+        else
+          raise InvalidValueError, "the \"#{item.unit_attr}\" is not valid for a date item unit attribute"
+        end
+        return next_event
+      end
+
+      def inc_year time
+        if time.year % 4 == 0
+          time + 60*60*24*366
+        else
+          time + 60*60*24*365
+        end
+      end
+
+      def inc_month time
+        increment = nil
+        if time.month == 2
+          if time.year % 4 == 0
+            increment = 60*60*24*29
+          else
+            increment = 60*60*24*28
+          end
+        elsif [1,3,5,7,8,10,12].include? time.month
+          increment = 60*60*24*31
+        else
+          increment = 60*60*24*30
+        end
+        fix_for_dst(time, time + increment)
+      end
+
+      def inc_week time
+        fix_for_dst(time, time + 60*60*24*7)
+      end
+
+      def inc_day
+        fix_for_dst(time, time + 60*60*24)
+      end
+
+      def inc_hour
+        kime + 3600
+      end
+
+      def fix_for_dst original_time, new_time
+        if original_time.hour != new_time.hour
+          new_time = new_time + (original_time.hour - new_time.hour) * 3600
+        end
+        if original_time.min != new_time.min
+          new_time = new_time + (original_time.min - new_time.min) * 60
+        end
+        new_time
+      end
+
   end
 end
 
---
+++ spec/ess/ess_spec.rb
@@ -0,0 +1,280 @@
+require 'spec_helper'
+
+module ESS
+  describe "ESS" do
+    let(:future_year) { Time.now.year + 2 }
+
+    context 'feed with one item with standalone date' do
+      let(:ess) do
+        ess = ESS.new
+        ess.channel.add_feed do |feed|
+          feed.title "Feed 1"
+          feed.dates.add_item do |item|
+            item.type_attr "standalone"
+            item.name "Date 1"
+            item.start Time.parse("#{future_year}-01-01 00:00")
+          end
+        end
+        ess
+      end
+
+      describe '#find_coming' do
+        it 'should return that one item in an array' do
+          feeds = ess.find_coming
+          feeds.length.should == 1
+          feeds[0][:feed].title.text!.should == "Feed 1"
+        end
+      end
+
+      describe '#find_between' do
+        it 'should return that one item in an array if it\'s between the two moments in time' do
+          feeds = ess.find_between(Time.parse("#{future_year-1}-12-31 23:59"), Time.parse("#{future_year}-01-01 00:01"))
+          feeds.length.should == 1
+          feeds[0][:feed].title.text!.should == "Feed 1"
+        end
+
+        it 'should return an empty array if the item event is not between the two moments in time' do
+          feeds = ess.find_between(Time.parse("#{future_year}-01-01 00:01"), Time.parse("#{future_year}-01-01 00:03"))
+          feeds.length.should == 0
+        end
+      end
+    end
+
+    context 'ESS channel with two feeds with future standalone dates' do
+      let(:ess) do
+        ess = ESS.new
+        ess.channel.add_feed do |feed|
+          feed.title "Feed 1"
+          feed.dates.add_item do |item|
+            item.type_attr "standalone"
+            item.name "Date 1"
+            item.start Time.parse("#{future_year}-01-01 00:00")
+          end
+        end
+        ess.channel.add_feed do |feed|
+          feed.title "Feed 2"
+          feed.dates.add_item do |item|
+            item.type_attr "standalone"
+            item.name "Date 1"
+            item.start Time.parse("#{future_year}-01-08 00:00")
+          end
+        end
+        ess
+      end
+
+      describe '#find_coming' do
+        it 'should return both items in an array if no parameters were specified' do
+          feeds = ess.find_coming
+          feeds.length.should == 2
+          feeds[0][:feed].title.text!.should == "Feed 1"
+          feeds[1][:feed].title.text!.should == "Feed 2"
+        end
+
+        it 'should return the first feed in an array if passed the number 1' do
+          feeds = ess.find_coming(1)
+          feeds.length.should == 1
+          feeds[0][:feed].title.text!.should == "Feed 1"
+        end
+
+        it 'should return both items in an array if passed the number 2' do
+          feeds = ess.find_coming(2)
+          feeds.length.should == 2
+          feeds[0][:feed].title.text!.should == "Feed 1"
+          feeds[1][:feed].title.text!.should == "Feed 2"
+        end
+
+        it 'should return both items in an array if passed the number 2, sorted by asc. date/time' do
+          feeds = ess.channel.feed_list
+          tmp1 = feeds[0]
+          tmp2 = feeds[1]
+          ess.channel.feed_list.clear
+          ess.channel.feed_list << tmp2
+          ess.channel.feed_list << tmp1
+          feeds = ess.find_coming(2)
+          feeds.length.should == 2
+          feeds[0][:feed].title.text!.should == "Feed 1"
+          feeds[1][:feed].title.text!.should == "Feed 2"
+        end
+        it 'should return both items in an array if passed the number 3' do
+          feeds = ess.find_coming(3)
+          feeds.length.should == 2
+          feeds[0][:feed].title.text!.should == "Feed 1"
+          feeds[1][:feed].title.text!.should == "Feed 2"
+        end
+      end
+
+      describe '#find_between' do
+        it 'should return an empty list if none of the events is between two moments in time' do
+          feeds = ess.find_between(Time.parse("#{future_year-1}-12-21 23:59"), Time.parse("#{future_year-1}-12-31 00:01"))
+          feeds.length.should == 0
+          feeds = ess.find_between(Time.parse("#{future_year}-01-10 23:59"), Time.parse("#{future_year}-01-13 00:01"))
+          feeds.length.should == 0
+        end
+
+        it 'should return the first feed if it is happening between the two dates' do
+          feeds = ess.find_between(Time.parse("#{future_year-1}-12-21 23:59"), Time.parse("#{future_year}-01-05 00:01"))
+          feeds.length.should == 1
+          feeds[0][:feed].title.text!.should == "Feed 1"
+        end
+
+        it 'should return the second feed if it is happening between the two dates' do
+          feeds = ess.find_between(Time.parse("#{future_year}-01-01 00:02"), Time.parse("#{future_year}-01-09 00:01"))
+          feeds.length.should == 1
+          feeds[0][:feed].title.text!.should == "Feed 2"
+        end
+
+        it 'should return both items if they both happen in the interval' do
+          feeds = ess.find_between(Time.parse("#{future_year}-01-01 00:00"), Time.parse("#{future_year}-01-09 00:01"))
+          feeds.length.should == 2
+          feeds[0][:feed].title.text!.should == "Feed 1"
+          feeds[1][:feed].title.text!.should == "Feed 2"
+        end
+      end
+    end
+
+    context 'one feed with two date items with standalone values' do
+      let(:ess) do
+        ess = ESS.new
+        ess.channel.add_feed do |feed|
+          feed.title "Feed 1"
+          feed.dates.add_item do |item|
+            item.type_attr "standalone"
+            item.name "Date 2"
+            item.start Time.parse("#{future_year}-01-01 00:15")
+          end
+          feed.dates.add_item do |item|
+            item.type_attr "standalone"
+            item.name "Date 1"
+            item.start Time.parse("#{future_year}-01-01 00:00")
+          end
+        end
+        ess
+      end
+
+      describe "#find_coming" do
+        it 'should return two items in a list, if no arguments are passed' do
+          ess.find_coming.length.should == 2
+        end
+
+        it 'should return one item in a list, if the only argument is a 1' do
+          ess.find_coming(1).length.should == 1
+        end
+
+        it 'should return a list of dicts, with keys :time and :feed' do
+          feeds = ess.find_coming
+          feeds.each do |feed|
+            feed[:time].should_not be_nil
+            feed[:feed].should_not be_nil
+          end
+        end
+
+        it 'should return a list of discts sorted by ascending order of time' do
+          feeds = ess.find_coming
+          feeds[0][:time].should == Time.parse("#{future_year}-01-01 00:00")
+          feeds[0][:feed].title.text!.should == "Feed 1"
+          feeds[1][:time].should == Time.parse("#{future_year}-01-01 00:15")
+          feeds[1][:feed].title.text!.should == "Feed 1"
+        end
+      end
+
+      describe "#find_between" do
+        it 'should return two items in a list, if both date items are within the time period' do
+          feeds = ess.find_between(Time.parse("#{future_year}-01-01 00:00"), Time.parse("#{future_year}-01-09 00:01"))
+          feeds.length == 2
+        end
+
+        it 'should return two hashes in a list, with :time and :feed values defined' do
+          feeds = ess.find_between(Time.parse("#{future_year}-01-01 00:00"), Time.parse("#{future_year}-01-09 00:01"))
+          feeds.each do |feed|
+            feed[:time].should_not be_nil
+            feed[:feed].should_not be_nil
+          end
+        end
+
+        it 'should return a list of dicts sorted by ascending order of time' do
+          feeds = ess.find_coming
+          feeds[0][:time].should == Time.parse("#{future_year}-01-01 00:00")
+          feeds[0][:feed].title.text!.should == "Feed 1"
+          feeds[1][:time].should == Time.parse("#{future_year}-01-01 00:15")
+          feeds[1][:feed].title.text!.should == "Feed 1"
+        end
+      end
+    end
+
+    context 'feed with one item with recurring date with a limit attribute' do
+      let(:ess) do
+        ess = ESS.new
+        ess.channel.add_feed do |feed|
+          feed.title "Feed 1"
+          feed.dates.add_item do |item|
+            item.type_attr "recurrent"
+            item.unit_attr "month"
+            item.limit_attr "5"
+            item.name "Date 1"
+            item.start Time.parse("#{future_year}-01-01 00:00")
+          end
+        end
+        ess
+      end
+
+      describe "#find_coming" do
+        it 'should return the array with as much elements, as there will be separate events' do
+          ess.find_coming.length.should == 5
+        end
+
+        it 'should return a list of feeds, sorted by starting date/time' do
+          feeds = ess.find_coming
+          feeds[0][:time].should == Time.parse("#{future_year}-01-01 00:00")
+          feeds[1][:time].should == Time.parse("#{future_year}-02-01 00:00")
+          feeds[2][:time].should == Time.parse("#{future_year}-03-01 00:00")
+          feeds[3][:time].should == Time.parse("#{future_year}-04-01 00:00")
+          feeds[4][:time].should == Time.parse("#{future_year}-05-01 00:00")
+        end
+      end
+
+      describe "#find_coming" do
+        it 'should return a list of event instances within time boundaries' do
+          feeds = ess.find_between(Time.parse("#{future_year}-02-11 00:00"), Time.parse("#{future_year}-07-11 12:00"))
+          feeds.length.should == 3
+          feeds[0][:time].should == Time.parse("#{future_year}-03-01 00:00")
+          feeds[1][:time].should == Time.parse("#{future_year}-04-01 00:00")
+          feeds[2][:time].should == Time.parse("#{future_year}-05-01 00:00")
+        end
+      end
+    end
+
+    context 'feed with one item with recurring date with a limit attribute and an interval attribute' do
+      let(:ess) do
+        ess = ESS.new
+        ess.channel.add_feed do |feed|
+          feed.title "Feed 1"
+          feed.dates.add_item do |item|
+            item.type_attr "recurrent"
+            item.unit_attr "month"
+            item.interval_attr "2"
+            item.limit_attr "5"
+            item.name "Date 1"
+            item.start Time.parse("#{future_year}-01-01 00:00")
+          end
+        end
+        ess
+      end
+
+      describe "#find_coming" do
+        it 'should return the array with as much elements, as there will be separate events' do
+          ess.find_coming.length.should == 5
+        end
+
+        it 'should return a list of feeds, sorted by starting date/time' do
+          feeds = ess.find_coming
+          feeds[0][:time].should == Time.parse("#{future_year}-01-01 00:00")
+          feeds[1][:time].should == Time.parse("#{future_year}-03-01 00:00")
+          feeds[2][:time].should == Time.parse("#{future_year}-05-01 00:00")
+          feeds[3][:time].should == Time.parse("#{future_year}-07-01 00:00")
+          feeds[4][:time].should == Time.parse("#{future_year}-09-01 00:00")
+        end
+      end
+    end
+  end
+end
+
